e7a8c6fc8a6e268d7412617cbda583dc41baf50b
Belli <66693949+Ivan-LB@users.noreply.github.com>
2022-09-10 15:06:28 -0700

    Update FirebaseDatabaseHelper.java

--- HeartBit/app/src/main/java/com/example/heartbit/FirebaseDatabaseHelper.java
+++ HeartBit/app/src/main/java/com/example/heartbit/FirebaseDatabaseHelper.java
@@ -1,7 +1,12 @@
 package com.example.heartbit;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +16,35 @@ public class FirebaseDatabaseHelper {
     private DatabaseReference mReferencePatients;
     private List<Patients> patients = new ArrayList<>();
 
+    public interface DataStatus{
+        void DataIsLoaded(List<Patients> patients, List<String> keys);
+        void DataIsInserted();
+        void DataIsUpdated();
+        void DataIsDeleted();
+    }
     public FirebaseDatabaseHelper(){
-        mDatabase =
+        mDatabase = FirebaseDatabase.getInstance();
+        mReferencePatients = mDatabase.getReference("patients");
+    }
+    public void readPatients(final DataStatus dataStatus){
+        mReferencePatients.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                patients.clear();
+                List<String> keys = new ArrayList<>();
+                for (DataSnapshot keyNode: dataSnapshot.getChildren()){
+                    keys.add(keyNode.getKey());
+                    Patients patient = keyNode.getValue(Patients.class);
+                    patients.add(patient);
+                }
+                dataStatus.DataIsLoaded(patients,keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
